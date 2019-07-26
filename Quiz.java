29babbc48462a05d534df980590e219472d34da6
Yaroslav Rybiak <yaroslav-rybiak@users.noreply.github.com>
2019-07-26 17:22:20 +0200

    Update Quiz.java

--- Quiz.java
+++ Quiz.java
@@ -3,16 +3,15 @@ import java.util.Scanner;
 import java.util.Random;
 
 class Quiz {
-    Quiz(){}
 
     public static void main(String[] args) {
         Dictionary dictionary = new Dictionary();
         dictionary.addLevel(1);
-//        dictionary.addLevel(2);
-//        dictionary.addLevel(3);
+        dictionary.addLevel(2);
+        dictionary.addLevel(3);
 
         ArrayList<String> answerDictionary = dictionary.getEnglishDictionary();
-        ArrayList<String> questionDictionary = dictionary.getJapaneseDictionary();
+        ArrayList<String> questionDictionary = dictionary.getKanaDictionary();
 
         Scanner scanner = new Scanner(System.in);
         boolean correctFlag = true;
@@ -20,11 +19,11 @@ class Quiz {
         int errorNumber = 0;
         ArrayList<String> errorList = new ArrayList<>();
 
-        while (!answerDictionary.isEmpty()) {
+        while (!questionDictionary.isEmpty()) {
             Random rand = new Random();
-            if(answerDictionary.size() > 1 && correctFlag) {
-                randomIndex = rand.nextInt(answerDictionary.size());
-            } else if(answerDictionary.size() == 1 && correctFlag) {
+            if(questionDictionary.size() > 1 && correctFlag) {
+                randomIndex = rand.nextInt(questionDictionary.size());
+            } else if(questionDictionary.size() == 1 && correctFlag) {
                 randomIndex = 0;
             }
             System.out.println(questionDictionary.get(randomIndex));
